b89889ad9885e76ade4f0b5918332995ef78359c
Nglittleguy <41524588+Nglittleguy@users.noreply.github.com>
2021-04-13 14:52:04 -0600

    main menu, pause, end

--- main.c
+++ main.c
@@ -21,11 +21,13 @@ int press;										//READ: main, WRITE: controller (up down left right start se
 int readPress;									//READ: controller, main, WRITE: controller (1), main (0),
 Game g;
 int cont = 1;									//READ: controller, main, WRITE: main
+int contGame = 0;
 const int timeLimit = 2000;
 int t = timeLimit; 								//READ: main, WRITE: timingClock
 int timeWait = 0;								//READ: main, controller, WRITE: main
 int timeTill = 0;								//READ: main, controller, WRITE: main
 int paused = 0;									//READ: main, controller, WRITE: main
+int mainScreen = 1;								//READ: main, controller, WRITE: main
 int lives = 5;
 int movesLeft = 500;
 int resetTime = 0;
@@ -43,14 +45,19 @@ const int bHeight = 100;
 void *timingClock(void *param) {
 	printf("Time %d\n", t);
 	while(cont) {
-		if(t==0 || movesLeft==0 || lives == 0) 
-			cont = 0;							//end the game
+
+		if((t==0 || movesLeft==0 || lives == 0) && contGame) {
+			contGame = 0;						//end the game
+			won = 0;
+		}
 		while(paused);
 		if(resetTime) {
-			//t = 500;
+			t = timeLimit;	
+			movesLeft = 500;
+			lives = 5;
 			resetTime = 0;
 		}
-		if(t%100==0 && powerUpOnScreen == 0) {
+		if(t<2000 && t%100==0 && powerUpOnScreen == 0) {
 			powerUpOnScreen = 1;
 		}
 		t--;
@@ -89,7 +96,7 @@ void *controls(void *param) {
 					break;
 				}
 				if(hold[i]==0 && readPress)	{	//if held spinlock is released and controller turn to write, 
-					printMessage(i);					//		print the message
+					printMessage(i);			//		print the message
 					press = i;
 					hold[i] = 1000;				//reset spinlock value
 					pressed = 1;				//if pressed, rewrite the "Press a button"
@@ -143,86 +150,205 @@ int main()
 	
 	framebufferstruct = initFbInfo();
 
-	generateGame(&g, width, height, widthBy24);		//ISSUE - constants not working
-	int levelChosen = 0;	
-	int currentLine = 23;
-	int updateLevel = 0;
-	int noPowerTimeYet = 1;
-	int oldLine = 23;
-	int goBackToLoop = 0;
+	
+	int levelChosen;	
+	int currentLine;
+	int updateLevel;
+	int noPowerTimeYet;
+	int oldLine;
+	int goBackToLoop;
+	int cursor = 0;
+	int restart = 0;
+	int won = 0;						//winning flag is 1, losing is 2, skip is 0
 	while(cont) {
-		if(updateLevel != 0) {					//inbetween levels
-			drawClearMem();
-			if(levelChosen<3)
-				levelChosen+=updateLevel;
-			else {
-				printf("WINNER!!!! %d - %d - %d", t, movesLeft, lives);
-				cont = 0;
-			}
-
-			if(powerUpOnScreen) {
-				removePowerUp(&g);
-				powerUpOnScreen = 0;
-			}
-
-			updateLevel = 0;
-			poweredUp = 0;
-			noPowerTimeYet = 1;
-			resetTime = 1;
-			goBackToLoop = 0;
-
-		}
-		if(!readPress) {
-			if(press == 3) {
-				paused = 1;						//enter pause menu here
-
-			}
-			if(!paused) {
-				oldLine = currentLine;
-				currentLine = movePlayer(&g, levelChosen, width, press, widthBy24, bWidth); 
-				if(oldLine == 0 && currentLine == 23){
-					updateLevel = 1;
-					goBackToLoop = 1;
+		while(mainScreen) {
+			//drawMainScreen(cursor);
+			if(!readPress) {
+				printf("Main menu, waiting for : %s", printMessage(press));
+				if(press==8) {			//press A
+					if(cursor) {		//Quit
+						drawClearMem();
+						drawBlank();
+						contGame = 0;
+						cont = 0;
+					}
+					else {				//Start Game
+						contGame = 1;
+						mainScreen = 0;
+					}
+					restart = 0;
 				}
-				else if(oldLine == 23 && currentLine == 0) {
-					updateLevel = -1;
-					goBackToLoop = 1;
+				else if(press==4){	//press Up 
+					printf("Start game");
+					if(cursor==1)
+						cursor=0;
 				}
-			}
-			readPress = 1;
-			if(goBackToLoop)
-				continue;
+				else if(press==5) {	//press Down
+					printf("Quit game")
+					if(cursor==0)
+						cursor=1;
+				}
+				readPress = 1;
+			}	
 		}
-		if(!paused) {
-			if(updateTime(&g, levelChosen, width, bWidth, widthBy24, currentLine)==0) {
-				// dead
-				lives--;
-				moveToStart(&g, levelChosen, width, widthBy24, currentLine);
-				currentLine = 23;
-			}
-			if(powerUpOnScreen) {
-				if(collectPowerUp(&g, currentLine, levelChosen, widthBy24)) {
+
+		//only called if !mainScreen
+		if(restart) {
+			restart = 0;
+			contGame = 1;
+		}
+
+		resetTime = 1;
+		levelChosen = 0;
+		currentLine = 23;
+		updateLevel = 0;
+		noPowerTimeYet = 1;
+		oldLine = 23;
+		goBackToLoop = 0;
+		generateGame(&g, width, height, widthBy24);
+
+		while(contGame) {
+			//In between Levels
+			if(updateLevel != 0) {	
+				drawClearMem();
+				if(levelChosen<3)
+					levelChosen+=updateLevel;
+				else {
+					//Congratulation, Winner!
+					printf("WINNER!!!! %d - %d - %d", t, movesLeft, lives);
+					if(contGame) {						//only 1 can change won flag (mutex)
+						won = 1;
+						contGame = 0;
+					}
+				}
+
+				if(powerUpOnScreen) {
 					removePowerUp(&g);
 					powerUpOnScreen = 0;
-					poweredUp = 1;
 				}
+
+				updateLevel = 0;
+				poweredUp = 0;
+				noPowerTimeYet = 1;
+				goBackToLoop = 0;
+
 			}
 
-		}
-		drawBackground(&g, levelChosen);
-		drawSprites(&g, levelChosen);
-		if(powerUpOnScreen) {
-			if(noPowerTimeYet) {
-				setUpPowerUp(&g, movesLeft, currentLine);
-				noPowerTimeYet = 0;
-			}
-			drawPowerUp(&g);
-		}
-		drawTotal();
-		drawTime(t, timeLimit);
-		delayMicroseconds(42);
+			//Reading an input
+			if(!readPress) {
+				if(press == 3) {
+					if(paused == 1)
+						paused = 0;
+					else {
+						paused = 1;						//enter pause menu here
+						cursor = 0;
+					}
+				}
 
+				//not paused
+				if(!paused) {
+					if(press!=3) {
+						oldLine = currentLine;
+						currentLine = movePlayer(&g, levelChosen, width, press, widthBy24, bWidth); 
+						if(oldLine == 0 && currentLine == 23){
+							updateLevel = 1;
+							goBackToLoop = 1;
+						}
+						else if(oldLine == 23 && currentLine == 0) {
+							updateLevel = -1;
+							goBackToLoop = 1;
+						}
+					}
+				}
+				//is paused
+				else {
+					if(press==8) {		//press A
+						if(cursor) {		//Quit
+							contGame = 0;
+							mainScreen = 1;
+						}
+						else {				//Restart
+							contGame = 0;
+							restart = 1;
+							mainScreen = 0;
+						}
+					}
+					else if(press==4){	//press Up
+						if(cursor==1) {
+							printf("Restart");
+							cursor=0;
+						}
+					}
+					else if(press==5) {	//press Down
+						if(cursor==0) {
+							printf("Quit");
+							cursor=1;
+						}
+					}
+				}
+				readPress = 1;
+				if(goBackToLoop)
+					continue;
+			}
+
+			//Responding to updates
+			if(!paused) {
+				if(updateTime(&g, levelChosen, width, bWidth, widthBy24, currentLine)==0) {
+					// dead
+					lives--;
+					moveToStart(&g, levelChosen, width, widthBy24, currentLine);
+					currentLine = 23;
+				}
+				if(powerUpOnScreen) {
+					if(collectPowerUp(&g, currentLine, levelChosen, widthBy24)) {
+						removePowerUp(&g);
+						powerUpOnScreen = 0;
+						poweredUp = 1;
+					}
+				}
+
+				//Draw the screen
+				drawBackground(&g, levelChosen);
+				drawSprites(&g, levelChosen);
+				if(powerUpOnScreen) {
+					if(noPowerTimeYet) {
+						setUpPowerUp(&g, movesLeft, currentLine);
+						noPowerTimeYet = 0;
+					}
+					drawPowerUp(&g);
+				}
+				drawTotal();
+				drawTime(t, timeLimit);
+			}
+			else {
+				//paused menu screen draw
+			}
+
+			
+
+			//Wait for next 1/24 
+			delayMicroseconds(42);
+		}
+
+		if(won!=0) {
+			if(won==1) {		//won the game
+				mainScreen = 1;
+				points += t+movesLeft+100*lives;
+				printf("Winner - won down there");
+				//DRAW STATS OF GAME WIN
+				
+			}
+			else if(won==2) {	//lost the game
+				//DRAW LOSER SCREEN
+				printf("Loser!! Haha!");
+				mainScreen = 1;
+			}
+			while(readPress);	//stay here until button press
+			readPress = 1;
+		}
 		
+		won = 0;
+
 	}
 
 	munmap(framebufferstruct.fptr, framebufferstruct.screenSize);
