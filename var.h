1729ae715192c1a02163fdc60d5b0e1b205238b1
Nglittleguy <41524588+Nglittleguy@users.noreply.github.com>
2021-04-19 13:19:59 -0600

    y then x

--- var.h
+++ var.h
@@ -4,3 +4,33 @@ extern const int width;
 extern const int widthBy24;
 extern const int bWidth;
 extern const int bHeight;
+
+extern const int s_0[35][20];
+extern const int s_1[35][20];
+extern const int s_2[35][20];
+extern const int s_3[35][20];
+extern const int s_4[35][20];
+extern const int s_5[35][20];
+extern const int s_6[35][20];
+extern const int s_7[35][20];
+extern const int s_8[35][20];
+extern const int s_9[35][20];
+extern const int s_colon[35][20];
+
+extern const int s_frog[30][50];
+
+extern const int s_valuePack[30][100];
+extern const int s_leftLawnmower[30][100];
+extern const int s_rightLawnmower[30][100];
+extern const int s_lilypad[30][100];
+extern const int s_log[30][100];
+extern const int s_rocket[30][100];
+extern const int s_satelite[30][100];
+extern const int s_carLeft[30][100];
+extern const int s_carRight[30][100];
+
+extern const int s_paused[570][760];
+extern const int s_quit[100][300];
+extern const int s_restart[100][300];
+
+
